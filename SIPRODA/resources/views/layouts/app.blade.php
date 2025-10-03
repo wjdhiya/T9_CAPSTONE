2606c5ed30f246dd3b84e5ff59bbfd764cd7ca8d
wj <hello.javvad@gmail.com>
2025-10-03 12:39:36 +0700

    done favicon1

--- SIPRODA/resources/views/layouts/app.blade.php
+++ SIPRODA/resources/views/layouts/app.blade.php
@@ -13,6 +13,7 @@
 
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
+        <link rel="icon" type="image/x-icon" href="{{ asset('favicon1.ico') }}">
     </head>
     <body class="font-sans antialiased">
         <div class="min-h-screen bg-gray-100">
